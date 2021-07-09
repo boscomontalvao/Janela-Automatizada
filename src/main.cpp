e8286c0d4bcd7fbdb2e520651ae0315d2a9abc36
João Bosco <boscomontalvao@hotmail.com.br>
2021-07-09 20:22:24 -0300

    Add automatização por horário

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,99 @@
-#include <Arduino.h>
+#include <NTPClient.h>
+#include <WiFiUDP.h>
+#include <ESP8266WiFi.h>
 
+WiFiUDP udp;
+NTPClient ntp(udp, "0.br.pool.ntp.org", -3 * 3600, 60000);
 
+const char* ssid = "Montalvao_2.4";
+const char* senha = "20071990";
 
-//definindo os os led que simulam a janela e o motor;
-//ledJanela == HIGH => janela aberta
-//ledJanela == LOW => janela fecha 
-//ledMovimento == HIGH => motor trabalhando
-//ledMovimento == LOW => motor parado
+const char* horaAbrir = "19:11:15";
+const char* horaFechar = "19:11:00";
+
 const byte ledMovimento = 2;
 const byte ledJanela = 16;
 const byte chave = 12;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(chave, INPUT);
-  pinMode(ledJanela, OUTPUT);
-  pinMode(ledMovimento, OUTPUT);
-  
+String horario;
+
+void ICACHE_RAM_ATTR ativarSensor();
+
+void setup()
+{
+   Serial.begin(9600);
+   
+   pinMode(chave, INPUT);
+   pinMode(ledJanela, OUTPUT);
+   pinMode(ledMovimento, OUTPUT);
+   digitalWrite(ledJanela, HIGH);
+   digitalWrite(ledMovimento, HIGH);
+   attachInterrupt(digitalPinToInterrupt(chave),ativarSensor,CHANGE);
+
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, senha);
+   Serial.print("Conecnato a rede: ");
+   Serial.println(ssid);
+   while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(500);
+   }
+   Serial.println("\nConectado com sucesso");
+
+   ntp.begin();
+   
+   Serial.print("\nAguardadndo sincronização");
+   while(!ntp.update()){
+      Serial.print(".");   
+      ntp.forceUpdate();
+      delay(500);
+   }
+   Serial.println("\nHorário sincronizado");
 }
+
 //simula o movimento de abrir a janela com o sinal do sensor de chuvar
 void movimentoAbrir() {
-  digitalWrite(ledMovimento, LOW);
-  delay(4000);
-  digitalWrite(ledMovimento, HIGH);
-  digitalWrite(ledJanela, LOW);
+   digitalWrite(ledMovimento, LOW);
+   digitalWrite(ledJanela, HIGH);
+   digitalWrite(ledMovimento, HIGH);
 }
 
 //simula o movimento de fechar a janela com o sinal do sensor de chuvar
 void movimentoFechar() {
-  digitalWrite(ledMovimento, LOW);
-  delay(4000);
-  digitalWrite(ledMovimento, HIGH);
-  digitalWrite(ledJanela, HIGH);
+   digitalWrite(ledMovimento, LOW);
+   digitalWrite(ledJanela, LOW);
+   digitalWrite(ledMovimento, HIGH);
 }
 
-void loop(){
-  //put your main code here, to run repeatedly:
-  if(digitalRead(chave) == HIGH){
-    if(digitalRead(ledJanela) == HIGH)
-      movimentoAbrir();
-  }
-  if(digitalRead(chave) == LOW){
-    if(digitalRead(ledJanela) == LOW)
-      movimentoFechar();
-  }
+void ICACHE_RAM_ATTR ativarSensor() {
+   if(digitalRead(chave) == LOW) {
+      if(digitalRead(ledJanela) == LOW)
+         movimentoAbrir();
+   }
+
+   if(digitalRead(chave) == HIGH) {
+      if(digitalRead(ledJanela) == HIGH)
+         movimentoFechar();
+   }
 }
 
+void loop() {
+   horario = ntp.getFormattedTime();
+   Serial.println(horario);
 
-  
+   if(horario == horaFechar) {
+      if(digitalRead(ledJanela) == HIGH){
+         movimentoFechar();
+      }
+   }
+
+   if (horario == horaAbrir) {
+      if(digitalRead(chave) == LOW) {
+         if(digitalRead(ledJanela) == LOW){
+            movimentoAbrir();
+         }  
+      }
+   }
+
+   delay(1000);
+}
